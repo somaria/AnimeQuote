c5423645de9e7a88c5d6fd8fda5a11355c2839bb
Daryl <daryl.9@gmail.com>
2021-08-25 18:54:29 +0800

    Initial Commit

---
+++ AnimeQuote/AnimeQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeQuoteApp.swift
+//  AnimeQuote
+//
+//  Created by admin on 25/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimeQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeQuote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimeQuote
+//
+//  Created by admin on 25/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
